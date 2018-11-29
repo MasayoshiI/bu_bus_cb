5e6164df60f587a74b3cbbe43bc0596b5b4f77a3
YOUR NAME <YOUR@EMAIL.com>
2018-11-29 14:57:20 -0500

    demo

--- hello/views.py
+++ hello/views.py
@@ -52,46 +52,37 @@ def webhook():
     r.headers['Content-Type'] = 'application/json'
     return r
 
-#processing the request from dialogflow
-#def processRequest(req):
-#
-#    result = req.get("result")
-#    parameters = result.get("parameters")
-#    city = parameters.get("geo-city")
-#    observation = owm.weather_at_place(city)
-#    w = observation.get_weather()
-#    latlon_res = observation.get_location()
-#    lat=str(latlon_res.get_lat())
-#    lon=str(latlon_res.get_lon())
-#
-#    wind_res=w.get_wind()
-#    wind_speed=str(wind_res.get('speed'))
-#
-#    humidity=str(w.get_humidity())
-#
-#    celsius_result=w.get_temperature('celsius')
-#    temp_min_celsius=str(celsius_result.get('temp_min'))
-#    temp_max_celsius=str(celsius_result.get('temp_max'))
-#
-#    fahrenheit_result=w.get_temperature('fahrenheit')
-#    temp_min_fahrenheit=str(fahrenheit_result.get('temp_min'))
-#    temp_max_fahrenheit=str(fahrenheit_result.get('temp_max'))
-#    speech = "Today the weather in " + city + ": \n" + "Temperature in Celsius:\nMax temp :"+temp_max_celsius+".\nMin Temp :"+temp_min_celsius+".\nTemperature in Fahrenheit:\nMax temp :"+temp_max_fahrenheit+".\nMin Temp :"+temp_min_fahrenheit+".\nHumidity :"+humidity+".\nWind Speed :"+wind_speed+"\nLatitude :"+lat+".\n  Longitude :"+lon
-#
-#    return {
-#        "speech": speech,
-#        "displayText": speech,
-#        "source": "dialogflow-weather-by-satheshrgs"
-#    }
-#
-#def index(request):
-#    r = requests.get('http://httpbin.org/status/418')
-#    print(r.text)
-#    return HttpResponse('<pre>' + r.text + '</pre>')
-#    # return HttpResponse('Hello from Python!')
-#    # return render(request, "index.html")
-#
+processing the request from dialogflow
+def processRequest(req):
 
+    result = req.get("result")
+    parameters = result.get("parameters")
+    city = parameters.get("geo-city")
+    observation = owm.weather_at_place(city)
+    w = observation.get_weather()
+    latlon_res = observation.get_location()
+    lat=str(latlon_res.get_lat())
+    lon=str(latlon_res.get_lon())
+
+    wind_res=w.get_wind()
+    wind_speed=str(wind_res.get('speed'))
+
+    humidity=str(w.get_humidity())
+
+    celsius_result=w.get_temperature('celsius')
+    temp_min_celsius=str(celsius_result.get('temp_min'))
+    temp_max_celsius=str(celsius_result.get('temp_max'))
+
+    fahrenheit_result=w.get_temperature('fahrenheit')
+    temp_min_fahrenheit=str(fahrenheit_result.get('temp_min'))
+    temp_max_fahrenheit=str(fahrenheit_result.get('temp_max'))
+    speech = "Today the weather in " + city + ": \n" + "Temperature in Celsius:\nMax temp :"+temp_max_celsius+".\nMin Temp :"+temp_min_celsius+".\nTemperature in Fahrenheit:\nMax temp :"+temp_max_fahrenheit+".\nMin Temp :"+temp_min_fahrenheit+".\nHumidity :"+humidity+".\nWind Speed :"+wind_speed+"\nLatitude :"+lat+".\n  Longitude :"+lon
+
+    return {
+        "speech": speech,
+        "displayText": speech,
+        "source": "dialogflow-weather-by-satheshrgs"
+    }
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
