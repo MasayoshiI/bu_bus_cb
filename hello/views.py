b6efef4ac5cce1cb4498a0b19be1a5483e85f4f0
YOUR NAME <YOUR@EMAIL.com>
2018-11-29 15:15:41 -0500

    demo

--- hello/views.py
+++ hello/views.py
@@ -7,8 +7,8 @@ from .models import Greeting
 # Create your views here.
 def index(request):
     r = requests.get('http://httpbin.org/status/418')
-    print(r.headers)
-    return HttpResponse('<pre>' + r.text + '</pre>')
+    pretty_print_POST(request);
+    return HttpResponse('<pre>' + r.text + '</pre>' + "index")
     # return HttpResponse('Hello from Python!')
     # return render(request, "index.html")
 
@@ -24,8 +24,26 @@ def db(request):
 
 def df(request):
     r = requests.get('http://httpbin.org/status/418')
-    print(r.headers)
-    return HttpResponse('<pre>' + r.text + '</pre>')
+    pretty_print_POST(request);
+    return HttpResponse('<pre>' + r.text + '</pre>'  + "df")
 # return HttpResponse('Hello from Python!')
 # return render(request, "index.html")
 
+req = requests.Request('POST','http://stackoverflow.com',headers={'X-Custom':'Test'},data='a=1&b=2')
+prepared = req.prepare()
+
+def pretty_print_POST(req):
+    """
+        At this point it is completely built and ready
+        to be fired; it is "prepared".
+        
+        However pay attention at the formatting used in
+        this function because it is programmed to be pretty
+        printed and may differ from the actual request.
+        """
+    print('{}\n{}\n{}\n\n{}'.format(
+        '-----------START-----------',
+        req.method + ' ' + req.url,
+        '\n'.join('{}: {}'.format(k, v) for k, v in req.headers.items()),
+        req.body,
+    ))
