8742b2b9f0e42e79b32350ea74c4c5d22d97a6c4
YOUR NAME <YOUR@EMAIL.com>
2018-11-29 16:20:23 -0500

    demo

--- hello/views.py
+++ hello/views.py
@@ -7,6 +7,60 @@ import json
 
 from .models import Greeting
 
+###### test JSON to return to DialogFlow request
+
+testJSON = {
+"speech": "this text is spoken out loud if the platform supports voice interactions",
+"displayText": "this text is displayed visually",
+"messages": {
+    "type": 1,
+        "title": "card title",
+            "subtitle": "card text",
+                "imageUrl": "https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png"
+},
+"data": {
+    "google": {
+    "expectUserResponse": true,
+    "richResponse": {
+        "items": [
+                  {
+                  "simpleResponse": {
+                  "textToSpeech": "this is a simple response"
+                  }
+                  }
+                  ]
+}
+    },
+        "facebook": {
+"text": "Hello, Facebook!"
+    },
+        "slack": {
+"text": "This is a text response for Slack."
+}
+},
+"contextOut": [
+               {
+               "name": "context name",
+               "lifespan": 5,
+               "parameters": {
+               "param": "param value"
+               }
+               }
+               ],
+"source": "example.com",
+"followupEvent": {
+    "name": "event name",
+        "parameters": {
+"param": "param value"
+}
+}
+
+}
+
+###### TESTJSON ENDS
+
+
+
 # Create your views here.
 @csrf_exempt
 def index(request):
@@ -37,8 +91,10 @@ def processPOST(request):
     print(request.method + " post ")
     body = json.loads(request.body)
     print(body)
+    json_response = json.dumps(testJSON)
     print(request.method + " done printing")
-    return HttpResponse('<pre>' + r.text + '</pre>' + "post")
+    return json_response
+#return HttpResponse('<pre>' + r.text + '</pre>' + "post")
 # return HttpResponse('Hello from Python!')
 # return render(request, "index.html")
 
