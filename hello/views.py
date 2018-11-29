68c3f197f2450125c2d2ac722c7771d93704ac02
YOUR NAME <YOUR@EMAIL.com>
2018-11-29 16:57:19 -0500

    demo

--- hello/views.py
+++ hello/views.py
@@ -13,17 +13,9 @@ testJSON = {
     "fulfillmentText": "This is a text response",
     "fulfillmentMessages": [
         {
-            "card": {
-            "title": "card title",
-            "subtitle": "card text",
-            "imageUri": "https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png",
-            "buttons": [
-                {
-                    "text": "button text",
-                    "postback": "https://assistant.google.com/"
-                }
+            "text": [
+                "you are great!"
             ]
-            }
         }
     ],
 }
