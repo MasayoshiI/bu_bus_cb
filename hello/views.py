50c60d757820bf79beac972ef2c8dd10b50dbb6f
YOUR NAME <YOUR@EMAIL.com>
2018-11-29 15:23:26 -0500

    demo

--- hello/views.py
+++ hello/views.py
@@ -7,7 +7,7 @@ from .models import Greeting
 # Create your views here.
 def index(request):
     r = requests.get('http://httpbin.org/status/418')
-    print(request.headers + " hey" )
+    print(request.method + " hey" )
     return HttpResponse('<pre>' + r.text + '</pre>' + "index")
     # return HttpResponse('Hello from Python!')
     # return render(request, "index.html")
