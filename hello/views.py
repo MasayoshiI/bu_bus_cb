24f8dd852df612fb577e6d38d0e2094f622ecb92
YOUR NAME <YOUR@EMAIL.com>
2018-11-29 16:08:12 -0500

    demo

--- hello/views.py
+++ hello/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 import requests
+import json
 
 
 from .models import Greeting
