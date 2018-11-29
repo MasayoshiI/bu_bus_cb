4a1da73f315662f8e1129e7a6756a7a580458023
YOUR NAME <YOUR@EMAIL.com>
2018-11-29 14:51:53 -0500

    demo

--- hello/views.py
+++ hello/views.py
@@ -21,7 +21,7 @@ def index(request):
     # return render(request, "index.html")
 
 @app.route('/')
-def dothis():
+def index():
     r = requests.get('http://httpbin.org/status/418')
     print(r.text)
     return HttpResponse('<pre>' + r.text + '</pre>')
