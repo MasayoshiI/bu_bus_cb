2e7b9caa4fbf3820700c57c46f48313ecad0f2c9
YOUR NAME <YOUR@EMAIL.com>
2018-11-30 16:12:41 -0500

    Demo

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "hello",
-    "busData",
 ]
 
 MIDDLEWARE = [
--- hello/views.py
+++ hello/views.py
@@ -3,8 +3,8 @@ from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 import requests
 import json
+import datetime
 from busData import get_estimate
-#from .models import Greeting
 
 
 
@@ -35,13 +35,6 @@ def index(request):
     else:
         return processPOST(request)
 
-##default function for heroku apps, function for /db URL
-#def db(request):
-#    greeting = Greeting()
-#    greeting.save()
-#    greetings = Greeting.objects.all()
-#    return render(request, "db.html", {"greetings": greetings})
-
 #test function for /df URL
 def df(request):
     r = requests.get('http://httpbin.org/status/418')
@@ -56,7 +49,7 @@ def processPOST(request):
     dict = {}
     if body["queryResult"]["parameters"]["next_bus"] == "next":
         bus_stop = body["queryResult"]["parameters"]["startingStation1"]
-        dict = busData.get_estimate(bus_stop)
+        dict = get_estimate(bus_stop)
     lowest = 1000
     for key in dict:
         if (key < lowest):
@@ -72,4 +65,56 @@ def processGET(request):
     r = requests.get('http://httpbin.org/status/418')
     print(request.method + " get")
     return HttpResponse('<pre>' + r.text + '</pre>' + "get")
+
+bus_stop_dict = {
+    "marsh_plaza":"4160734",
+    "cfa":"4160738",
+    "stuvi":"4160714",
+    "amory":"4114006",
+    "stmary":"4149154",
+    "blandford":"4068466",
+    "hotel":"4068470",
+    "huntingtonm6":"4110206",
+    "albany":"4068482",
+    "huntingtonc2":"4160718",
+    "danielsen":"4160722",
+    "myles":"4160726",
+    "silber":"4160730"
+}
+
+data = {}
+
+#establish connection to bu bus server data, load json into program for processing
+def read_bus_data():
+    global data
+    r = requests.get('https://www.bu.edu/bumobile/rpc/bus/livebus.json.php', auth=('user', 'pass'))
+    #print(r.status_code, " <-- if 200, successful connection")
+    data = json.loads(r.text)
+
+#takes in bus stop string, returns a dictionary {minutes until next arrival:bus_route}#
+def get_estimate(stop_str):
+    read_bus_data()
+    ret = {}
+    stop_id = bus_stop_dict[stop_str]
+    for bus in data["ResultSet"]["Result"]:
+        estimates = bus.get("arrival_estimates")
+        #print(bus["id"], " ", bus["route"])
+        if(estimates != None):
+            for stops in estimates:
+                if(stops["stop_id"] == stop_id):
+                    time_until = calculate_time_diff(stops["arrival_at"])
+                    ret[time_until] = bus["route"]
+    print(ret)
+
+#helper function for get_estimate; calculates minutes until arrival
+def calculate_time_diff(bus_time):
+    current = datetime.datetime.now()
+    bus_time_obj = datetime.datetime.strptime(bus_time, '%Y-%m-%dT%H:%M:%S-05:00')
+    return (bus_time_obj - current).seconds/60
+#    print(current, " bus time", bus_time_obj)
+#    print(bus_time_obj - current)
+
+
+read_bus_data()
+get_estimate("danielsen")
 
